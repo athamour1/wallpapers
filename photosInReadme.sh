52d7e4f90851d8a3fbc8afe536721312bae5ce0c
athamour1 <thanosmour.tk@gmail.com>
2022-08-26 11:47:26 +0300

    Compress photos for preview

--- photosInReadme.sh
+++ photosInReadme.sh
@@ -1,15 +1,18 @@
 a=1
 for i in wallpapers/*.jpg; do
-  echo "![]($i)" >> README.md
+  cwebp -q 40 $i -o samples/$a.webp
+  echo "![](samples/$a.webp)" >> README.md
   let a=a+1
 done
 b=a
 for j in wallpapers/*.png; do
-  echo "![]($j)" >> README.md
+  cwebp -q 40 $i -o samples/$b.webp
+  echo "![](samples/$b.webp)" >> README.md
   let b=b+1
 done
 c=b
 for n in wallpapers/*.png; do
-  echo "![]($n)" >> README.md
+  cwebp -q 40 $i -o samples/$c.webp
+  echo "![](samples/$c.webp)" >> README.md
   let c=c+1
 done
